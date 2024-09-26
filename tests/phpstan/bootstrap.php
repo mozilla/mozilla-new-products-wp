125ac6bb0828bf2fbf7f039cc76e394825d0ef69
Josh Pensky <joshuapensky@gmail.com>
2024-09-26 09:31:40 -0400

    Rename from wsk to mozilla-builders

--- tests/phpstan/bootstrap.php
+++ tests/phpstan/bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 
 define( 'WP_ENV', 'testing' );
-define( 'WSK_THEME_URL', 'https://' );
-define( 'WSK_THEME_PATH', './' );
-define( 'WSK_DOMAIN', 'https://' );
-define( 'WSK_SITE_NAME', 'Site name' );
-define( 'WSK_THEME_VERSION', '0.0.0' );
+define( 'MOZILLA_BUILDERS_THEME_URL', 'https://' );
+define( 'MOZILLA_BUILDERS_THEME_PATH', './' );
+define( 'MOZILLA_BUILDERS_DOMAIN', 'https://' );
+define( 'MOZILLA_BUILDERS_SITE_NAME', 'Site name' );
+define( 'MOZILLA_BUILDERS_THEME_VERSION', '0.0.0' );
