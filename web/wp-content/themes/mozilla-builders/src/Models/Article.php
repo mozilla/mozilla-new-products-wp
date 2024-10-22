f432851b8c1d1a1cb427671502ed814c6d7f0d8e
Nick Braica <nick.braica@gmail.com>
2024-10-22 11:43:57 -0400

    Check for empty author

--- web/wp-content/themes/mozilla-builders/src/Models/Article.php
+++ web/wp-content/themes/mozilla-builders/src/Models/Article.php
@@ -20,6 +20,10 @@ class Article extends TimberPost {
 	 */
 	public function authors(): array {
 		$authors = $this->meta( 'authors' );
+		if ( empty( $authors ) ) {
+			return array();
+		}
+
 		return array_map( fn( $author ) => Timber::get_post( $author ), $authors );
 	}
 }
