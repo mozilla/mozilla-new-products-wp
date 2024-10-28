498c8af02099a32a5bcd3d784cdc288be49c61cd
Josh Pensky <joshuapensky@gmail.com>
2024-10-28 12:28:37 -0400

    wip

--- web/wp-content/themes/mozilla-builders/tailwind.config.js
+++ web/wp-content/themes/mozilla-builders/tailwind.config.js
@@ -18,25 +18,32 @@ for (let i = 1; i <= GRID_COLUMNS; i++) {
   gridColumnEnd[`end-${i}`] = i;
 }
 
+function color(name) {
+  return `rgb(var(--color-${name}) / <alpha-value>)`;
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./**/*.twig'],
-  darkMode: 'class',
   theme: {
     colors: {
+      // Base colors
       current: 'currentColor',
       transparent: 'transparent',
-      black: '#000000',
-      white: '#ffffff',
-      gray: '#F0F0F0',
+      black: color('black'),
+      white: color('white'),
+      gray: color('gray'),
       green: {
-        DEFAULT: '#00D230',
-        dark: '#005E2A',
+        DEFAULT: color('green'),
+        dark: color('green-dark'),
       },
-      orange: '#FF9900',
-      yellow: '#FFDD63',
-      purple: '#7F00CA',
-      pink: '#FF008A',
+      orange: color('orange'),
+      yellow: color('yellow'),
+      purple: color('purple'),
+      pink: color('pink'),
+      // Applied colors
+      main: color('main'),
+      content: color('content'),
     },
 
     fontFamily: {
