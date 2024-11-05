c0e43f2267eda188389f229cbc7110ba20b6f195
Josh Pensky <joshuapensky@gmail.com>
2024-11-05 15:06:13 -0500

    v1

---
+++ web/wp-content/themes/mozilla-builders/blocks/advanced-code/advanced-code.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Custom block for inserting code with syntax highlighting.
+ *
+ * @package MozillaBuilders
+ * @param array $block The block settings and attributes.
+ */
+
+use Timber\Timber;
+use Highlight\Highlighter;
+
+$alignment = $block['align'] ?? 'wide';
+
+$hl = new Highlighter();
+$code = get_field( 'code' );
+$language = get_field( 'language' );
+
+$context             = Timber::context();
+$context['code']     = array(
+	'plain' => $code,
+	'highlighted' => $hl->highlight( $language, $code )->value,
+);
+$context['language'] = $language;
+$context['alignclass'] = "align$alignment";
+
+Timber::render( basename( __DIR__ ) . '/advanced-code.twig', $context );
