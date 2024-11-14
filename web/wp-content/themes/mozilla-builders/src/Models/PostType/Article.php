21d36fef8314700f412d90a230e3aefee6822213
Josh Pensky <joshuapensky@gmail.com>
2024-11-14 12:35:03 -0500

    wip

--- web/wp-content/themes/mozilla-builders/src/Models/PostType/Article.php
+++ web/wp-content/themes/mozilla-builders/src/Models/PostType/Article.php
@@ -26,4 +26,30 @@ class Article extends TimberPost {
 
 		return array_map( fn( $author ) => Timber::get_post( $author ), $authors );
 	}
+
+	/**
+	 * Related articles for a post.
+	 *
+	 * @return array
+	 */
+	public function related_articles(): array {
+		$selected_articles = $this->meta( 'related_articles' );
+		if ( empty( $selected_articles ) ) {
+			$selected_articles = array();
+		}
+		$count = 4 - count( $selected_articles );
+		if ( $count > 0 ) {
+			$args = array(
+				'post_type' => 'post',
+				'posts_per_page' => $count,
+				'post__not_in' => array_merge( $selected_articles, array( $this->ID ) ),
+				'orderby' => 'date',
+				'order' => 'DESC',
+			);
+			$pulled_articles = Timber::get_posts( $args );
+			$selected_articles = array_merge( $selected_articles, $pulled_articles->to_array() );
+		}
+
+		return array_map( fn( $article ) => Timber::get_post( $article ), $selected_articles );
+	}
 }
