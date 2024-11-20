9898bd010fa8dce3fc4a8ed2aeeb6abc44c3e44d
Josh Pensky <joshuapensky@gmail.com>
2024-11-20 11:24:36 -0500

    wip

--- web/wp-content/themes/mozilla-builders/front-page.php
+++ web/wp-content/themes/mozilla-builders/front-page.php
@@ -21,39 +21,36 @@ $context['topper'] = $topper;
 
 $featured_post_ids = $topper['featured_articles'];
 if ( empty( $featured_post_ids ) ) {
-	$featured_post_ids = array();
+	$context['featured_posts'] = array();
+} else {
+	$context['featured_posts'] = Timber::get_posts(
+		array(
+			'post_type'      => 'post',
+			'posts_per_page' => 6,
+			'post_status'    => 'publish',
+			'post__in'       => $featured_post_ids,
+			'orderby'        => 'post__in',
+		)
+	);
 }
-$_posts                    = Timber::get_posts(
-	array(
-		'post_type'      => 'post',
-		'posts_per_page' => 6,
-		'post_status'    => 'publish',
-		'post__in'       => $featured_post_ids,
-		'orderby'        => 'post__in',
-	)
-);
-$context['featured_posts'] = $_posts;
 
 $builders            = $_page->meta( 'builders' );
 $context['builders'] = $builders;
 
 $featured_people_ids = $builders['people'];
 if ( empty( $featured_people_ids ) ) {
-	$featured_people_ids = array();
+	$context['featured_people'] = array();
+} else {
+	$context['featured_people'] = Timber::get_posts(
+		array(
+			'post_type'      => Profile::HANDLE,
+			'posts_per_page' => 6,
+			'post_status'    => 'publish',
+			'post__in'       => $featured_people_ids,
+			'orderby'        => 'post__in',
+		)
+	);
 }
-$_people                    = Timber::get_posts(
-	array(
-		'post_type'      => Profile::HANDLE,
-		'posts_per_page' => 6,
-		'post_status'    => 'publish',
-		'post__in'       => $featured_people_ids,
-		'orderby'        => 'post__in',
-	)
-);
-$context['featured_people'] = $_people;
-
-$collaborations            = $_page->meta( 'collaborations' );
-$context['collaborations'] = $collaborations;
 
 $secondary_post_ids = $_page->meta( 'secondary_featured_articles' );
 if ( empty( $secondary_post_ids ) ) {
@@ -70,20 +67,23 @@ if ( empty( $secondary_post_ids ) ) {
 	);
 }
 
+$collaborations            = $_page->meta( 'collaborations' );
+$context['collaborations'] = $collaborations;
+
 $featured_project_ids = $collaborations['projects'];
 if ( empty( $featured_project_ids ) ) {
-	$featured_project_ids = array();
+	$context['featured_projects'] = array();
+} else {
+	$context['featured_projects'] = Timber::get_posts(
+		array(
+			'post_type'      => Project::HANDLE,
+			'posts_per_page' => 4,
+			'post_status'    => 'publish',
+			'post__in'       => $featured_project_ids,
+			'orderby'        => 'post__in',
+		)
+	);
 }
-$_projects                    = Timber::get_posts(
-	array(
-		'post_type'      => Project::HANDLE,
-		'posts_per_page' => 4,
-		'post_status'    => 'publish',
-		'post__in'       => $featured_project_ids,
-		'orderby'        => 'post__in',
-	)
-);
-$context['featured_projects'] = $_projects;
 
 $_latest_posts             = Timber::get_posts(
 	array(
