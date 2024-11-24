531a4bc3d43723beca41f32ba405001ef400f447
Andy Rader <andy.rader@upstatement.com>
2024-11-24 15:02:07 +0100

    Adds header and text block

---
+++ web/wp-content/themes/mozilla-builders/blocks/header-text/header-text.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * A custom block for inserting a side by side header and text.
+ *
+ * @package MozillaBuilders
+ * @param array $block The block settings and attributes.
+ */
+
+use Timber\Timber;
+
+$context                 = Timber::context();
+$context['size']         = get_field( 'block_size' );
+$context['header_text']  = get_field( 'header_text' );
+$context['text_content'] = get_field( 'text_content' );
+
+Timber::render( basename( __DIR__ ) . '/header-text.twig', $context );
