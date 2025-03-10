7ed43f9c1b6d94fbc57fb9dc1ac66f6dfb6fe941
gvjacob <gvjacob@outlook.com>
2025-03-10 12:53:41 -0400

    Remove video embed cover on play

--- web/wp-content/themes/mozilla-labs/static/js/app.js
+++ web/wp-content/themes/mozilla-labs/static/js/app.js
@@ -16,19 +16,22 @@ import { links } from '@src/plugins/links';
 import { marquee } from '@src/plugins/marquee';
 import { masonry } from '@src/plugins/masonry';
 import { tabs } from '@src/plugins/tabs';
+import { videoEmbed } from '@src/plugins/video-embed';
 
 // Initialize Alpine
 window.Alpine = Alpine;
 // Register Alpine plugins
-Alpine.plugin(accelerator);
-Alpine.plugin(accordion);
-Alpine.plugin(clipboard);
-Alpine.plugin(focus);
-Alpine.plugin(hangPunctuation);
-Alpine.plugin(headingNav);
-Alpine.plugin(links);
-Alpine.plugin(marquee);
-Alpine.plugin(masonry);
-Alpine.plugin(tabs);
+Alpine.plugin( accelerator );
+Alpine.plugin( accordion );
+Alpine.plugin( clipboard );
+Alpine.plugin( focus );
+Alpine.plugin( hangPunctuation );
+Alpine.plugin( headingNav );
+Alpine.plugin( links );
+Alpine.plugin( marquee );
+Alpine.plugin( masonry );
+Alpine.plugin( tabs );
+Alpine.plugin( videoEmbed );
+
 // Start Alpine
 Alpine.start();
