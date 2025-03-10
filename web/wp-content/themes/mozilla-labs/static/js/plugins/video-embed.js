5dcc73f846467c79ac093118d1ea8322609c5fbe
gvjacob <gvjacob@outlook.com>
2025-03-10 13:56:52 -0400

    Fix js formatting issue

--- web/wp-content/themes/mozilla-labs/static/js/plugins/video-embed.js
+++ web/wp-content/themes/mozilla-labs/static/js/plugins/video-embed.js
@@ -1,34 +1,34 @@
-export function videoEmbed( Alpine ) {
-  Alpine.directive( 'video-embed', ( el, { value } ) => {
-    if ( value === 'cover' ) {
-      handleCover( el, Alpine );
-    } else if ( value === 'play' ) {
-      handlePlay( el, Alpine );
+export function videoEmbed(Alpine) {
+  Alpine.directive('video-embed', (el, { value }) => {
+    if (value === 'cover') {
+      handleCover(el, Alpine);
+    } else if (value === 'play') {
+      handlePlay(el, Alpine);
     } else {
-      handleRoot( el, Alpine );
+      handleRoot(el, Alpine);
     }
-  } );
+  });
 }
 
-function handleRoot( el, Alpine ) {
-  Alpine.bind( el, {
+function handleRoot(el, Alpine) {
+  Alpine.bind(el, {
     'x-data'() {
       return {
         playing: false,
       };
     },
-  } );
+  });
 }
-function handleCover( el, Alpine ) {
-  Alpine.bind( el, {
+function handleCover(el, Alpine) {
+  Alpine.bind(el, {
     ':data-playing'() {
       return this.playing;
     },
-  } );
+  });
 }
 
-function handlePlay( el, Alpine ) {
-  Alpine.bind( el, {
+function handlePlay(el, Alpine) {
+  Alpine.bind(el, {
     '@click'() {
       this.playing = true;
     },
@@ -36,5 +36,5 @@ function handlePlay( el, Alpine ) {
     ':data-playing'() {
       return this.playing;
     },
-  } );
+  });
 }
