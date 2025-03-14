7318cf0ccb76c424f2be02c9b9b57adaef809cb0
gvjacob <gvjacob@outlook.com>
2025-03-14 13:54:49 -0400

    Typewriter effect

--- web/wp-content/themes/mozilla-labs/static/js/plugins/typewriter.js
+++ web/wp-content/themes/mozilla-labs/static/js/plugins/typewriter.js
@@ -1,3 +1,9 @@
+// Speed to rotate to the next text.
+const ROTATION_SPEED = 3000;
+
+// Speed to type out the text.
+const TYPEWRITER_SPEED = 100;
+
 export function typewriter(Alpine) {
   Alpine.directive('typewriter', (el, { value, expression }) => {
     if (value === 'text') {
@@ -12,12 +18,22 @@ function handleRoot(el, Alpine) {
   Alpine.bind(el, {
     'x-data'() {
       return {
-        typing: false,
+        index: 0, // Index of the current text to type out.
+        texts: [], // Collection of all texts.
+        typing: false, // Whether the current text is being typed out.
+
+        next() {
+          this.index = (this.index + 1) % this.texts.length;
+        },
+
+        isCurrentText(textEl) {
+          return this.texts[this.index] === textEl;
+        },
       };
     },
 
     ':aria-busy'() {
-      return this.typing;
+      return this.typing ? 'true' : 'false';
     },
   });
 }
@@ -26,25 +42,49 @@ function handleText(el, Alpine, text) {
   Alpine.bind(el, {
     'x-data'() {
       return {
-        character: 0,
+        characterIndex: 0,
+        interval: null,
+
+        reset() {
+          this.characterIndex = 0;
+          clearInterval(this.interval);
+        },
+
+        type() {
+          if (this.isCurrentText(el)) {
+            this.typing = true;
+
+            this.interval = setInterval(() => {
+              if (this.characterIndex >= text.length) {
+                this.typing = false;
+                this.reset();
+
+                setTimeout(() => {
+                  el.innerHTML = '';
+                  this.next();
+                }, ROTATION_SPEED);
+
+                return;
+              }
+
+              el.innerHTML += text[this.characterIndex];
+              this.characterIndex++;
+            }, TYPEWRITER_SPEED);
+          }
+        },
       };
     },
 
     'x-init'() {
       el.innerHTML = '';
+      this.texts = this.texts.concat(el);
 
-      const interval = setInterval(() => {
-        this.typing = true;
+      Alpine.watch(
+        () => this.index,
+        () => this.type(),
+      );
 
-        if (this.character >= text.length) {
-          clearInterval(interval);
-          this.typing = false;
-          return;
-        }
-
-        el.innerHTML += text[this.character];
-        this.character++;
-      }, 500);
+      this.type();
     },
   });
 }
