28b260ee51dd9d8d7c94afbb7affbfd335f6f7a2
gvjacob <gvjacob@outlook.com>
2025-03-17 16:24:47 -0400

    Delete unused javascript

--- web/wp-content/themes/mozilla-labs/static/js/app.js
+++ web/wp-content/themes/mozilla-labs/static/js/app.js
@@ -7,30 +7,19 @@ import '../scss/app.scss';
 
 import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
-import { accelerator } from '@src/plugins/accelerator';
 import { accordion } from '@src/plugins/accordion';
-import { clipboard } from '@src/plugins/clipboard';
 import { hangPunctuation } from '@src/plugins/hang-punctuation';
-import { headingNav } from '@src/plugins/heading-nav';
 import { links } from '@src/plugins/links';
-import { marquee } from '@src/plugins/marquee';
-import { masonry } from '@src/plugins/masonry';
-import { tabs } from '@src/plugins/tabs';
 import { videoEmbed } from '@src/plugins/video-embed';
 
 // Initialize Alpine
 window.Alpine = Alpine;
+
 // Register Alpine plugins
-Alpine.plugin(accelerator);
 Alpine.plugin(accordion);
-Alpine.plugin(clipboard);
 Alpine.plugin(focus);
 Alpine.plugin(hangPunctuation);
-Alpine.plugin(headingNav);
 Alpine.plugin(links);
-Alpine.plugin(marquee);
-Alpine.plugin(masonry);
-Alpine.plugin(tabs);
 Alpine.plugin(videoEmbed);
 
 // Start Alpine
